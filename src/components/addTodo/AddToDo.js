84b860f259f609c2cda161b54d753e82ad714c22
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-05 01:15:13 +0600

    todo added with nanoid

--- src/components/addTodo/AddToDo.js
+++ src/components/addTodo/AddToDo.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from "@reduxjs/toolkit";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../../redux/slices/todoSlice";
 const AddToDo = () => {
-    const dispatch = useDispatch();
-    const state = useSelector((state) => state);
-    console.log(state);
-    return (
-        <div>
-            
-        </div>
+  const dispatch = useDispatch();
+  const {entities} = useSelector((state) => state.todos);
+  const [text, setText] = useState("");
+  console.log(entities);
+  const submit = () => {
+    dispatch(
+      addTodo({
+        id: nanoid(),
+        todo: text,
+        completed: false,
+      })
     );
+  };
+  return (
+    <div style={{ margin: "50px" }}>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <button onClick={submit}>Add</button>
+    </div>
+  );
 };
 
 export default AddToDo;

d385a8c042c523acc83e302c67f3c99674a8f156
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-05 00:04:02 +0600

    todoSlice added

---
+++ src/redux/slices/todoSlices.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+ const todoSlices = createSlice({
+    name: "todo",
+    initialState:[],
+    reducers: {
+
+    }
+})
+
+export default todoSlices;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../slices/todoSlices";
 
 export const store = configureStore({
     reducer:{
-        
+        todos: todoReducer
     }
 })

6f4c8c7ae625aa7e007452f193fed84ae5c668b8
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-08 05:50:41 +0600

    todo

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, updateTodo } from "../../../store/todoSlice";
 
-const Todo = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Todo = ({ text, completed, id }) => {
+  const dispatch = useDispatch();
+  const toggleItem = () => {
+    dispatch(updateTodo({ id, changes: { completed: !completed } }));
+  };
+  const deleteItem = () => {
+    dispatch(deleteTodo(id));
+  };
+  return (
+    <div className="todo">
+      <input type="checkbox" value={completed} onChange={toggleItem} />
+      <span>{text}</span>
+      <button onClick={deleteItem}>x</button>
+    </div>
+  );
 };
 
 export default Todo;
